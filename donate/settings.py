0913990ef3dd42c7811b3d303caeb474dd68caa2
John Kyalo Kitheka <84455190+jkyalo-go@users.noreply.github.com>
2021-07-24 18:45:37 +0300

    Update settings.py

--- donate/settings.py
+++ donate/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p5u$qe3&nuh#)-h!8nwqt14zmmscg_ld!&ou3(bd_9t5ruedxs'
+SECRET_KEY = 'YOUR_SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
